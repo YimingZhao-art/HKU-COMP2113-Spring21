1e68e3d5a6deeb43978eab226417148fbdc18d96
YimingZhao-art <78484762+YimingZhao-art@users.noreply.github.com>
2021-04-02 21:15:55 +0800

    Update data.h

--- data.h
+++ data.h
@@ -1,4 +1,4 @@
-#include <string>
+#include<string>
 using namespace std;
 
 #ifndef DATA_H
@@ -11,10 +11,11 @@ struct MAGICS //Store the inf of magics.
     string damage; //The exact damage is between [ 0.8*damage , 1.2* damage ] randomly.
 };
 
+
 struct POKEMON //Store the inf of pokemon.
 {
     string name; //name of each pokemon.
-    int level;  //level of each pokemon, from 1 to 10.
+    int level = 1;  //level of each pokemon, from 1 to 10.
     int hp; //The maximum HP of a Pokemon should follow: HPmax = 24 * level. The hp variable here just means current hp.
     int hpmax;
     string magics = "nnnn"; //For each of the four places means a magics from the Magic base. n means none. 0 to 9 is different 10 magics.
@@ -22,21 +23,23 @@ struct POKEMON //Store the inf of pokemon.
     int experience = 0; //Level up when experience > 100, then reset to 0 each time.
 };
 
+
 struct BOSS //Store the inf of Boss.
 {
     string name; //name of each boss.
     int level = 10; //The default level of boss is 10;
     int hp; //The maximum HP of Boss, follow: HPmax = 40 * level.  The hp variable here just means current hp.
     int hpmax = 400;
-    string magics = "6789"; //Boss will use default magics.
+    string magics="6789"; //Boss will use default magics.
     string character; //The characteristic of a boss. i.e. fire water wood normal.
 };
 
 struct USER //Store the inf of user.
-{   
-    string name; // user name to save
-    int money; //The money of user.
-    bool status[4]; //True for captured a pokemon, false for not.
+{
+    string name; // for reading
+    int money = 100; //The money of user.
+    int number_of_pokeman = 0;
+    POKEMON * bag = new POKEMON [number_of_pokeman];
     int train = 0; //The number of train can be add to a pokemon to train it.
 };
 
