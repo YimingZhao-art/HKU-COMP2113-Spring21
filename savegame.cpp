3735b09c2861cd3dd07530a61eef3bddb01d1eff
YimingZhao-art <78484762+YimingZhao-art@users.noreply.github.com>
2021-04-02 21:16:44 +0800

    Add files via upload
    存档

---
+++ savegame.cpp
@@ -0,0 +1,29 @@
+//This file is used to be the main file of the game
+//Following are libraries we need to use.
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+#include<cstdio>
+#include<ctime>
+#include<string>
+#include<cctype>
+//Following are libraries defined by us.
+#include "data.h"
+#include "initial.h"
+
+using namespace std;
+
+void save(USER user){
+    string filename = user.name + ".txt";
+    
+    ofstream file( filename.c_str() );
+    
+    if ( file.fail() )
+        return;
+    file << user.name << ' ' <<  user.money << ' ' << user.train <<' ' << user.number_of_pokeman << endl;
+    for ( int i = 0; i < user.number_of_pokeman; i++ ){
+        file << user.bag[i].name << ' ' << user.bag[i].level << ' ' << user.bag[i].magics << ' ' << user.bag[i].experience << endl;
+    }
+    
+    file.close();
+}
---
+++ savegame.h
@@ -0,0 +1,11 @@
+#include <string>
+#include "data.h"
+
+using namespace std;
+
+#ifndef SAVEGAME_H
+#define SAVEGAME_H
+
+void save(USER user);
+
+#endif
