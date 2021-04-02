f540a1e34208242fa56dd4ac408ff3c230a518ea
YimingZhao-art <78484762+YimingZhao-art@users.noreply.github.com>
2021-04-02 20:16:15 +0800

    Update data.h
    小改动

--- data.h
+++ data.h
@@ -17,9 +17,9 @@ struct POKEMON //Store the inf of pokemon.
     int level;  //level of each pokemon, from 1 to 10.
     int hp; //The maximum HP of a Pokemon should follow: HPmax = 24 * level. The hp variable here just means current hp.
     int hpmax;
-    string magics="nnnn"; //For each of the four places means a magics from the Magic base. n means none. 0 to 9 is different 10 magics.
+    string magics = "nnnn"; //For each of the four places means a magics from the Magic base. n means none. 0 to 9 is different 10 magics.
     string character; //The characteristic of a pokemon. i.e. fire water wood normal.
-    int experience; //Level up when experience > 100, then reset to 0 each time.
+    int experience = 0; //Level up when experience > 100, then reset to 0 each time.
 };
 
 struct BOSS //Store the inf of Boss.
@@ -28,7 +28,7 @@ struct BOSS //Store the inf of Boss.
     int level = 10; //The default level of boss is 10;
     int hp; //The maximum HP of Boss, follow: HPmax = 40 * level.  The hp variable here just means current hp.
     int hpmax = 400;
-    string magics="6789"; //Boss will use default magics.
+    string magics = "6789"; //Boss will use default magics.
     string character; //The characteristic of a boss. i.e. fire water wood normal.
 };
 
