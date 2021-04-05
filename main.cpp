67ac9c11c1362bfdfaf658db8ce8b5e570d038b3
YimingZhao-art <78484762+YimingZhao-art@users.noreply.github.com>
2021-04-05 15:37:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@
 //Following are libraries defined by us.
 #include "data.h"
 #include "initial.h"
+#include "savegame.h"
+#include "readgame.h"
+
+
 
 using namespace std;
 
@@ -18,6 +22,16 @@ BOSS boss[4];
 POKEMON pokemons[4];
 USER user;
 
+
+void display1() {
+    cout << "------------------------------------------\n" <<
+            "Choose one of the modes: \n" <<
+            "loadgame\n" <<
+            "newgame\n" <<
+            "exit\n" <<
+            "------------------------------------------\n";
+}
+
 int main()
 {
     initialmagics(magics); //Initialize the data of magics.
@@ -25,27 +39,53 @@ int main()
     initialpokemon(pokemons); //Initial the data of Pokemons.
     
     display1(); //When first open the game, display the menu for user to choose: load game/new game/exit.
-    while(1) //Used when first open the program.
+    while ( 1 ) //Used when first open the program.
     {   
         string get;
-        cout<<"Instruction:"<<endl;
+        cout << "Instruction:" <<endl;
         cin >> get; //Get the instruction from the user.
-        if (get == "loadgame")
+        
+        
+        if ( get == "loadgame" )
         {
-            loadgame(); //Call the function to load the game.
+            cout << "Input your user name: ";
+            cin >> user.name;
+            read(user); //Call the function to load the game.
             break;
         }
-        else if (get == "newgame")
+        
+        
+        else if ( get == "newgame" )
         {
-            newgame(); //Call the function to create new game.
+            //newgame(); //Call the function to create new game.
+            cout << "Input your user name: ";
+            cin >> user.name;
+            user.money = 100;
+            cout << "Choose one of the pokemons(input from 0-3):\n ";
+            for ( int i = 0; i < 4; i++ ){
+                cout << i << ": " << pokemons[i].name <<endl;
+            }
+            
+            int x;
+            cin >> x;
+            
+            user.number_of_pokeman = 1;
+            user.bag[0] = pokemons[x];
+            
+            
             break;
         }
-        else if (get == "exit")
+        
+        
+        else if ( get == "exit" ){
+            save(user);
             return 0; //shutdown the game.
+        }
+        
         else
-            cout<<"Wrong instruction! Please try again(loadgame, newgame, exit)."<<endl;  
+            cout << "Wrong instruction! Please try again(loadgame, newgame, exit)." << endl;
     }
-    
+        
     //The following part would be main body of the program.
     
     return 0;
